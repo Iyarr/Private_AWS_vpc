880a93260bf332adf5bbda40360017923bfdc0ec
Iyarr <8.24ridiculous@gmail.com>
2024-09-04 21:18:54 +0900

    #1 file:プレフィックスの追加

--- .sh
+++ .sh
@@ -4,7 +4,7 @@ yum install -y unzip amazon-cloudwatch-agent
 script /var/log/test.log
 cd /opt/aws/amazon-cloudwatch-agent
 echo "$agent_json" > ./etc/amazon-cloudwatch-agent.json
-./bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ./etc/amazon-cloudwatch-agent.json -s
+./bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:./etc/amazon-cloudwatch-agent.json -s
 su - ec2-user
 cd ~
 curl https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.20.03.zip -o server.zip
